aaf643940017e952cb09ddedbe1103e1609477e9
LuquiGole <golelucas@gmail.com>
2022-10-07 15:27:04 -0300

    Registramos las classes dentro de admin.py

--- ProyectoFinal/Blog/admin.py
+++ ProyectoFinal/Blog/admin.py
@@ -3,4 +3,6 @@ from Blog.models import Autor, Articulo, Seccion
 
 # Register your models here.
 
-admin.site.
+admin.site.register(Autor)
+admin.site.register(Articulo)
+admin.site.register(Seccion)
--- ProyectoFinal/Blog/models.py
+++ ProyectoFinal/Blog/models.py
@@ -4,16 +4,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     profesion = models.CharField(max_length=50)
-    
+
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=1000)
     fecha_publicacion = models.DateField(null=True)
-    
+
+
 class Seccion(models.Model):
     nombre = models.CharField(max_length=50)
