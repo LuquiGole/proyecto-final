f509e3b41d68ccf261d0a486f020d955ea28294f
LuquiGole <golelucas@gmail.com>
2022-10-07 16:28:16 -0300

    Templates + primer funcion

--- ProyectoFinal/Blog/views.py
+++ ProyectoFinal/Blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from Blog.models import Autor, Articulo, Seccion
 
 # Create your views here.
+
+
+def inicio(request):
+    return render(request, "inicio.html")
